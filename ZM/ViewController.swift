7d15d6fcea1da80f53c339180813f573a7b11e98
Muhammad <abdurazakov96@mail.ru>
2019-09-14 19:33:56 +0300

    Initial Commit

---
+++ ZM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZM
+//
+//  Created by Магомед Абдуразаков on 14/09/2019.
+//  Copyright © 2019 Магомед Абдуразаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
